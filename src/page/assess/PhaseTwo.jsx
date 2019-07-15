52872a88dcf1c399f6234b70ebc8b9a15967ca6e
jiguoqing0112 <93326792@qq.com>
2019-07-15 22:44:49 +0800

    up

--- src/page/assess/PhaseTwo.jsx
+++ src/page/assess/PhaseTwo.jsx
@@ -97,8 +97,26 @@ class PhoseTwo extends Component {
         if (index === 2) {
           obj.props.rowSpan = 2;
         }
+        if (index === 3) {
+          obj.props.rowSpan = 0;
+        }
+        if (index === 4) {
+          obj.props.rowSpan = 2;
+        }
+        if (index === 5) {
+          obj.props.rowSpan = 0;
+        }
+        if (index === 8) {
+          obj.props.rowSpan = 2;
+        }
+        if (index === 9) {
+          obj.props.rowSpan = 0;
+        }
+        if (index === 10) {
+          obj.props.rowSpan = 4;
+        }
         // These two are merged into above cell
-        if (index ===3 ||index ===4 ||index === 5||index === 6||index === 7) {
+        if (index ===11 ||index ===12 ||index === 13) {
           obj.props.rowSpan = 0;
         }
         return obj;
@@ -145,63 +163,13 @@ class PhoseTwo extends Component {
       },
     },
   ];
-  const data = [
-    {
-      key: '1',
-      content: '工作态度',
-      point: '认真和积极的学习、工作态度',
-      standard: '按时按量保质的发送每日学习报告，Shadow Daily Report，Shadow Summary Report 和阶段性总结报告',
-      percent: 5,
-      score: 2,
-    },
-    {
-      key: '1',
-      content: '工作态度',
-      point: '认真和积极的学习、工作态度',
-      standard: 'Shadow过程中Leader对工作态度的评估分数',
-      percent: 5,
-      score: 3,
-    },
-    {
-      key: '2',
-      content: '胜任能力',
-      point: '学习能力',
-      standard:"",
-      percent: 3,
-      score: 3,
-    },
-    {
-      key: '3',
-      content: '胜任能力',
-      point: '表达能力',
-      standard:"",
-      percent: 3,
-      score: 4,
-    },
-    {
-      key: '4',
-      content: '胜任能力',
-      point: '处理问题能力',
-      standard:"",
-      percent: 6,
-      score: 5,
-    },
-    {
-      key: '5',
-      content: 'Jake White',
-      point: 18,
-      standard:"",
-      percent: 5,
-      score: 4,
-    },
-  ];  
     return (
       <div >
         <Table
           onRowClick={self.handleRowClick}
           style={{ marginTop: '8px' }}
           pagination={false}
-          dataSource={data}
+          dataSource={this.state.assessTwo}
           columns={columns}
           bordered
           size="middle"
